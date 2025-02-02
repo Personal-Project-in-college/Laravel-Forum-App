ccd0d7327a3768f8326e6b9500d73642e3153e32
Farhan <118242884+FarhanGinting@users.noreply.github.com>
2025-02-02 14:12:04 +0700

    feat (Table) : finished creating the Like table structure, configuring uuid in the model, and finally creating dummy data.

---
+++ app/Models/Like.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Like extends Model
+{
+    /** @use HasFactory<\Database\Factories\LikeFactory> */
+    use HasUuids, HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    
+    protected $fillable = [
+        'post_id',
+        'user_id',
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             TagSeeder::class,
             PostSeeder::class,
             CommentSeeder::class,
+            LikeSeeder::class,
         ]);
     }
 }
